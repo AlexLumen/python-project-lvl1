0d3d036c84243b97a0211b9af2f778ccec8c56a1
aleksey.vedin@rt.ru <SonyXperia5567>
2021-07-26 20:54:41 +0400

    add division

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,19 @@
+import random
+
+rules = "Find the greatest common divisor of given numbers."
+
+
+def prepare_question_and_calculate():
+    first_number = random.randint(1, 100)
+    second_number = random.randint(1, 100)
+    numerator = max(first_number, second_number)
+    denominator = min(first_number, second_number)
+    quotient = numerator % denominator
+    while quotient != 0:
+        numerator = denominator
+        denominator = quotient
+        quotient = numerator % denominator
+    nod = denominator
+    true_answer = str(nod)
+    question = (str(first_number), str(second_number))
+    return true_answer, question
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-from brain_games.games import even
+from brain_games.games import gcd
 from brain_games.games_logic import play
 
 
 def main():
-    play(module=even)
+    play(module=gcd)
