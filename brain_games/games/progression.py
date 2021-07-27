f566398d5a1262773799718d4c929aea5fa0a856
aleksey.vedin@rt.ru <SonyXperia5567>
2021-07-27 15:27:00 +0400

    add progression game

---
+++ brain_games/games/progression.py
@@ -0,0 +1,25 @@
+import random
+
+rules = "What number is missing in the progression?"
+
+
+def prepare_question_and_calculate():
+    progression_list = []
+    number = random.randint(1, 20)
+    iterator = random.randint(1, 10)
+    for _ in range(10):
+        progression_list.append(str(number))
+        number += iterator
+    elem_for_replace = progression_list[random.randint(1, len(progression_list))]
+    for (index, elem) in enumerate(progression_list):
+        if progression_list[index] == elem_for_replace:
+            progression_list[index] = '..'
+            break
+    progression = ''
+    for (index, elem) in enumerate(progression_list):
+        progression += progression_list[index]
+        progression += ' '
+    progression = progression.rstrip(' ')
+    question = progression
+    true_answer = elem_for_replace
+    return true_answer, question
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from brain_games.games import progression
+from brain_games.games_logic import play
+
+
+def main():
+    play(module=progression)
