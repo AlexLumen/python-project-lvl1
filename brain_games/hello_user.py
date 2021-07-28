76c0475aeaaceb5d900933f32adcf71e3bd9e90e
aleksey.vedin@rt.ru <SonyXperia5567>
2021-07-28 14:08:31 +0400

    fix

--- brain_games/hello_user.py
+++ brain_games/hello_user.py
@@ -5,5 +5,5 @@ def hello_user():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     if name != '':
-        print(f'Hello, {name}!')
+        print(f'Hello,{name}!')
     return name
